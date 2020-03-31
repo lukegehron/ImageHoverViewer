df45c7167cc4e37a41771dde637de9b0d4097994
lukegehron <luke@gehron.com>
2020-03-31 09:18:18 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,273 @@
+let img1;
+let img2;
+let img3;
+let img4;
+let img5;
+let img6;
+let img7;
+let img11;
+let img21;
+let img31;
+let img41;
+let img51;
+let img61;
+let img71;
+let img12;
+let img22;
+let img32;
+let img42;
+let img52;
+let img62;
+let img72;
+let img13;
+let img23;
+let img33;
+let img43;
+let img53;
+let img63;
+let img73;
+let img14;
+let img24;
+let img34;
+let img44;
+let img54;
+let img64;
+let img74;
+let img15;
+let img25;
+let img35;
+let img45;
+let img55;
+let img65;
+let img75;
+let img16;
+let img26;
+let img36;
+let img46;
+let img56;
+let img66;
+let img76;
+
+
+
+function preload() {
+  img1 = loadImage("Animotion000.jpg");
+  img2 = loadImage("Animotion001.jpg");
+  img3 = loadImage("Animotion002.jpg");
+  img4 = loadImage("Animotion003.jpg");
+  img5 = loadImage("Animotion004.jpg");
+  img6 = loadImage("Animotion005.jpg");
+  img7 = loadImage("Animotion006.jpg");
+
+  img11 = loadImage("Animotion013.jpg");
+  img21 = loadImage("Animotion012.jpg");
+  img31 = loadImage("Animotion011.jpg");
+  img41 = loadImage("Animotion010.jpg");
+  img51 = loadImage("Animotion009.jpg");
+  img61 = loadImage("Animotion008.jpg");
+  img71 = loadImage("Animotion007.jpg");
+
+  img12 = loadImage("Animotion014.jpg");
+  img22 = loadImage("Animotion015.jpg");
+  img32 = loadImage("Animotion016.jpg");
+  img42 = loadImage("Animotion017.jpg");
+  img52 = loadImage("Animotion018.jpg");
+  img62 = loadImage("Animotion019.jpg");
+  img72 = loadImage("Animotion020.jpg");
+
+  img13 = loadImage("Animotion027.jpg");
+  img23 = loadImage("Animotion026.jpg");
+  img33 = loadImage("Animotion025.jpg");
+  img43 = loadImage("Animotion024.jpg");
+  img53 = loadImage("Animotion023.jpg");
+  img63 = loadImage("Animotion022.jpg");
+  img73 = loadImage("Animotion021.jpg");
+
+  img14 = loadImage("Animotion028.jpg");
+  img24 = loadImage("Animotion029.jpg");
+  img34 = loadImage("Animotion030.jpg");
+  img44 = loadImage("Animotion031.jpg");
+  img54 = loadImage("Animotion032.jpg");
+  img64 = loadImage("Animotion033.jpg");
+  img74 = loadImage("Animotion034.jpg");
+
+  img15 = loadImage("Animotion041.jpg");
+  img25 = loadImage("Animotion040.jpg");
+  img35 = loadImage("Animotion039.jpg");
+  img45 = loadImage("Animotion038.jpg");
+  img55 = loadImage("Animotion037.jpg");
+  img65 = loadImage("Animotion036.jpg");
+  img75 = loadImage("Animotion035.jpg");
+
+
+  img16 = loadImage("Animotion042.jpg");
+  img26 = loadImage("Animotion043.jpg");
+  img36 = loadImage("Animotion044.jpg");
+  img46 = loadImage("Animotion045.jpg");
+  img56 = loadImage("Animotion046.jpg");
+  img66 = loadImage("Animotion047.jpg");
+  img76 = loadImage("Animotion048.jpg");
+}
+
+function setup() {
+  createCanvas(1024, 523);
+}
+
+
+
+function draw() {
+  if (mouseY > 0 && mouseY < 76 && mouseX > 878 && mouseX < 1024) {
+    image(img76, 0, 0);
+  }
+  if (mouseY > 76 && mouseY < 152 && mouseX > 878 && mouseX < 1024) {
+    image(img75, 0, 0);
+  }
+  if (mouseY > 152 && mouseY < 228 && mouseX > 878 && mouseX < 1024) {
+    image(img74, 0, 0);
+  }
+  if (mouseY > 228 && mouseY < 304 && mouseX > 878 && mouseX < 1024) {
+    image(img73, 0, 0);
+  }
+  if (mouseY > 304 && mouseY < 380 && mouseX > 878 && mouseX < 1024) {
+    image(img72, 0, 0);
+  }
+  if (mouseY > 380 && mouseY < 456 && mouseX > 878 && mouseX < 1024) {
+    image(img71, 0, 0);
+  }
+  if (mouseY > 456 && mouseY < 532 && mouseX > 878 && mouseX < 1024) {
+    image(img7, 0, 0);
+  }
+
+
+  if (mouseY > 0 && mouseY < 76 && mouseX > 731 && mouseX < 878) {
+    image(img66, 0, 0);
+  }
+  if (mouseY > 76 && mouseY < 152 && mouseX > 731 && mouseX < 878) {
+    image(img65, 0, 0);
+  }
+  if (mouseY > 152 && mouseY < 228 && mouseX > 731 && mouseX < 878) {
+    image(img64, 0, 0);
+  }
+  if (mouseY > 228 && mouseY < 304 && mouseX > 731 && mouseX < 878) {
+    image(img63, 0, 0);
+  }
+  if (mouseY > 304 && mouseY < 380 && mouseX > 731 && mouseX < 878) {
+    image(img62, 0, 0);
+  }
+  if (mouseY > 380 && mouseY < 456 && mouseX > 731 && mouseX < 878) {
+    image(img61, 0, 0);
+  }
+  if (mouseY > 456 && mouseY < 532 && mouseX > 731 && mouseX < 878) {
+    image(img6, 0, 0);
+  }
+
+  if (mouseY > 0 && mouseY < 76 && mouseX > 585 && mouseX < 731) {
+    image(img56, 0, 0);
+  }
+  if (mouseY > 76 && mouseY < 152 && mouseX > 585 && mouseX < 731) {
+    image(img55, 0, 0);
+  }
+  if (mouseY > 152 && mouseY < 228 && mouseX > 585 && mouseX < 731) {
+    image(img54, 0, 0);
+  }
+  if (mouseY > 228 && mouseY < 304 && mouseX > 585 && mouseX < 731) {
+    image(img53, 0, 0);
+  }
+  if (mouseY > 304 && mouseY < 380 && mouseX > 585 && mouseX < 731) {
+    image(img52, 0, 0);
+  }
+  if (mouseY > 380 && mouseY < 456 && mouseX > 585 && mouseX < 731) {
+    image(img51, 0, 0);
+  }
+  if (mouseY > 456 && mouseY < 532 && mouseX > 585 && mouseX < 731) {
+    image(img5, 0, 0);
+  }
+
+  if (mouseY > 0 && mouseY < 76 && mouseX > 439 && mouseX < 585) {
+    image(img46, 0, 0);
+  }
+  if (mouseY > 76 && mouseY < 152 && mouseX > 439 && mouseX < 585) {
+    image(img45, 0, 0);
+  }
+  if (mouseY > 152 && mouseY < 228 && mouseX > 439 && mouseX < 585) {
+    image(img44, 0, 0);
+  }
+  if (mouseY > 228 && mouseY < 304 && mouseX > 439 && mouseX < 585) {
+    image(img43, 0, 0);
+  }
+  if (mouseY > 304 && mouseY < 380 && mouseX > 439 && mouseX < 585) {
+    image(img42, 0, 0);
+  }
+  if (mouseY > 380 && mouseY < 456 && mouseX > 439 && mouseX < 585) {
+    image(img41, 0, 0);
+  }
+  if (mouseY > 456 && mouseY < 532 && mouseX > 439 && mouseX < 585) {
+    image(img4, 0, 0);
+  }
+
+  if (mouseY > 0 && mouseY < 76 && mouseX > 293 && mouseX < 439) {
+    image(img36, 0, 0);
+  }
+  if (mouseY > 76 && mouseY < 152 && mouseX > 293 && mouseX < 439) {
+    image(img35, 0, 0);
+  }
+  if (mouseY > 152 && mouseY < 228 && mouseX > 293 && mouseX < 439) {
+    image(img34, 0, 0);
+  }
+  if (mouseY > 228 && mouseY < 304 && mouseX > 293 && mouseX < 439) {
+    image(img33, 0, 0);
+  }
+  if (mouseY > 304 && mouseY < 380 && mouseX > 293 && mouseX < 439) {
+    image(img32, 0, 0);
+  }
+  if (mouseY > 380 && mouseY < 456 && mouseX > 293 && mouseX < 439) {
+    image(img31, 0, 0);
+  }
+  if (mouseY > 456 && mouseY < 532 && mouseX > 293 && mouseX < 439) {
+    image(img3, 0, 0);
+  }
+
+  if (mouseY > 0 && mouseY < 76 && mouseX > 146 && mouseX < 293) {
+    image(img26, 0, 0);
+  }
+  if (mouseY > 76 && mouseY < 152 && mouseX > 146 && mouseX < 293) {
+    image(img25, 0, 0);
+  }
+  if (mouseY > 152 && mouseY < 228 && mouseX > 146 && mouseX < 293) {
+    image(img24, 0, 0);
+  }
+  if (mouseY > 228 && mouseY < 304 && mouseX > 146 && mouseX < 293) {
+    image(img23, 0, 0);
+  }
+  if (mouseY > 304 && mouseY < 380 && mouseX > 146 && mouseX < 293) {
+    image(img22, 0, 0);
+  }
+  if (mouseY > 380 && mouseY < 456 && mouseX > 146 && mouseX < 293) {
+    image(img21, 0, 0);
+  }
+  if (mouseY > 456 && mouseY < 532 && mouseX > 146 && mouseX < 293) {
+    image(img2, 0, 0);
+  }
+
+  if (mouseY > 0 && mouseY < 76 && mouseX > 0 && mouseX < 146) {
+    image(img16, 0, 0);
+  }
+  if (mouseY > 76 && mouseY < 152 && mouseX > 0 && mouseX < 146) {
+    image(img15, 0, 0);
+  }
+  if (mouseY > 152 && mouseY < 228 && mouseX > 0 && mouseX < 146) {
+    image(img14, 0, 0);
+  }
+  if (mouseY > 228 && mouseY < 304 && mouseX > 0 && mouseX < 146) {
+    image(img13, 0, 0);
+  }
+  if (mouseY > 304 && mouseY < 380 && mouseX > 0 && mouseX < 146) {
+    image(img12, 0, 0);
+  }
+  if (mouseY > 380 && mouseY < 456 && mouseX > 0 && mouseX < 146) {
+    image(img11, 0, 0);
+  }
+  if (mouseY > 456 && mouseY < 532 && mouseX > 0 && mouseX < 146) {
+    image(img1, 0, 0);
+  }
+}
